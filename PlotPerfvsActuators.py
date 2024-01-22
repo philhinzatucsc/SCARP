368a6796afd2b54577ec4cb30d8b07e2d232f1f3
philhinzatucsc <phinz@ucsc.edu>
2024-01-22 10:40:56 -0800

    added plotting scripts

---
+++ PlotPerfvsActuators.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+matrix = np.loadtxt('data/PerfvsActuators.txt',skiprows=1,delimiter=',')
+
+mask = (matrix[:,7] == 0.32)
+actuators = matrix[mask,11]
+open_FWHM = matrix[mask,12]
+av_FWHM = matrix[mask,13]
+gain_032 = open_FWHM / av_FWHM 
+
+mask_2 = (matrix[:,7] == 0.8)
+actuators_2 = matrix[mask_2,11]
+open_FWHM_2 = matrix[mask_2,12]
+av_FWHM_2 = matrix[mask_2,13]
+gain_080 = open_FWHM_2 / av_FWHM_2 
+
+plt.plot(actuators,gain_032, '*',label='0.32 um')
+plt.plot(actuators_2,gain_080, '*',label='0.8 um')
+
+#plt.plot(actuators,av_FWHM, 'o',color='C0')
+plt.ylim([1,3])
+plt.xlabel('Number of Actuators')
+plt.ylabel('Gain in Averaged FWHM over 9 arcmin square')
+plt.legend()
+plt.savefig('plots/PerfvsActuators.png')
+#plt.show()
